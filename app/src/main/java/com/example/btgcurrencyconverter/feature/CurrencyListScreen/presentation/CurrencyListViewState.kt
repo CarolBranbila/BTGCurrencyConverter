01f3a58e010fa27cd87d4cf6119c16358d8d7245
Carol Branbila <carolbranbila@hotmail.com>
2025-05-20 19:09:09 +0100

    Create CurrencyViewState

---
+++ app/src/main/java/com/example/btgcurrencyconverter/feature/CurrencyListScreen/presentation/CurrencyListViewState.kt
@@ -0,0 +1,9 @@
+package com.example.btgcurrencyconverter.feature.CurrencyListScreen.presentation
+
+data class CurrencyListViewState(
+    val list: List <Currency>
+)
+
+data class Currency(
+    val name : String
+)
