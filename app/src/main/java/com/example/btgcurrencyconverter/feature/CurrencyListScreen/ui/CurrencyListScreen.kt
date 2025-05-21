4d5895f76c9b0d68366fa5c219bf04135da65d4a
Carol Branbila <carolbranbila@hotmail.com>
2025-05-21 17:09:06 +0100

    Update CurrencyListScreen

--- app/src/main/java/com/example/btgcurrencyconverter/feature/CurrencyListScreen/ui/CurrencyListScreen.kt
+++ app/src/main/java/com/example/btgcurrencyconverter/feature/CurrencyListScreen/ui/CurrencyListScreen.kt
@@ -4,28 +4,58 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.btgcurrencyconverter.feature.CurrencyListScreen.presentation.Currency
+import com.example.btgcurrencyconverter.feature.CurrencyListScreen.presentation.CurrencyListViewState
 import com.example.btgcurrencyconverter.ui.theme.BTGCurrencyConverterTheme
+
+@Composable
+fun CurrencyListScreen(
+    modifier: Modifier = Modifier,
+    list: List<Currency>,
+    onClick: (Currency) -> Unit
+) {
+    LazyColumn(
+        modifier = modifier
+            .padding(16.dp)
+    ) {
+        items(
+            items = list,
+            key = null,
+        ) {
+            CurrencyItem(
+                name = it.name,
+                isSelected = it.isSelected,
+                onClick = { onClick (it)},
+            )
+        }
+    }
+}
 
 @Composable
 fun CurrencyItem(
     modifier: Modifier = Modifier,
     name: String,
     isSelected: Boolean,
-    onClick: () -> Unit,
+    onClick: (Currency) -> Unit
 ) {
     Row(
         modifier = modifier.border(
-                width = 1.dp,
-                shape = MaterialTheme.shapes.medium,
-                color = MaterialTheme.colorScheme.secondaryContainer,
-            ),
+            width = 1.dp,
+            shape = MaterialTheme.shapes.medium,
+            color = MaterialTheme.colorScheme.secondaryContainer,
+        ),
         verticalAlignment = Alignment.CenterVertically,
     ) {
         Text(
@@ -59,6 +89,6 @@ private fun CurrencyItemPreview() {
                 onClick = { },
             )
         }
-        
+
     }
 }
