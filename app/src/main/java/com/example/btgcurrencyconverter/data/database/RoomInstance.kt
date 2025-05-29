580095cf4b0e97e3ead4e0517da8926d06572ad2
Carol Branbila <carolbranbila@hotmail.com>
2025-05-29 19:51:43 +0100

    Create AppDataBase e RoomInstance

---
+++ app/src/main/java/com/example/btgcurrencyconverter/data/database/RoomInstance.kt
@@ -0,0 +1,12 @@
+package com.example.btgcurrencyconverter.data.database
+
+import android.content.Context
+import androidx.room.Room
+
+fun createRoomDatabaseInstance(
+    applicationContext: Context
+) =
+    Room.databaseBuilder(
+        applicationContext,
+        AppDataBase::class.java, "database-name"
+    ).build()
